721df49df0234bd43be559889451dc3a7e7f4fe3
meron <malemu17@yahoo.com>
2020-02-11 20:27:01 -0800

    loginMeron

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
-
+import LoginForm from './components/Login';
 
 function App() {
+  
   return (
     <div className="App">
-     <p>Chat App</p>
+     <p>Our chat app is:</p>
+     
+     <LoginForm />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
