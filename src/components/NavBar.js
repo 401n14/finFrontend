67d6fc9f848bad58daa917717582bbce0506f4de
Martin Balke <Mrtnbalke@gmail.com>
2020-02-15 10:53:29 -0800

    added a chat route

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -48,6 +48,16 @@ const NavBar = () => {
                   Home
                 </NavLink>
               </NavItem>
+              <NavItem>
+                <NavLink
+                  tag={RouterNavLink}
+                  to="/chat"
+                  exact
+                  activeClassName="router-link-exact-active"
+                >
+                  Chat
+                </NavLink>
+              </NavItem>
             </Nav>
             <Nav className="d-none d-md-block" navbar>
               {!isAuthenticated && (
