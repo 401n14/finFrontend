ecc33a8f236661688b991d9adf3a007bcfaba116
Martin Balke <Mrtnbalke@gmail.com>
2020-02-20 20:15:42 -0800

    username fix

--- src/components/ChatMessages.js
+++ src/components/ChatMessages.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useAuth0 } from "../react-auth0-spa";
 function ChatMessages(props) {
-    const { user} = useAuth0();
     return (
         <div className='overflow'>{props.children && props.children.length ? props.children.map((item, ind) => {
             let itemArray= (item.split('PM' || 'AM'));
             let timestamp = itemArray[0];
             let message = itemArray[1].split(':')[1];
+            let user = itemArray[1].split(':')[0];
+            console.log(user);
 
             return (
                 <div key={ind} className={`${props.className}-information`}>
@@ -14,10 +14,10 @@ function ChatMessages(props) {
                         <div
                             className={`${props.className}-information-avatar`}
                             style={{
-                                backgroundImage: `url(${user.picture})`
+                                backgroundImage: `url()`
                             }}
                         />
-                        <h3 className={`${props.className}-information-username`}>{user.nickname}</h3>
+                        <h3 className={`${props.className}-information-username`}>{user}</h3>
                         <p className={`${props.className}-information-timestamp`}>{timestamp}</p>
                     </div>
                     <p className={`${props.className}-information-message`}>
