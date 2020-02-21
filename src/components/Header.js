e830af15bc29082fbbe4e7c4c5d2d20b7bef4c85
Anna Li <annalynn1124@gmail.com>
2020-02-20 19:07:45 -0800

    adding comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,10 @@
 import React from 'react';
-
+/**
+ * Component for displaying header - when user joins/leaves the chat
+ * @component Header
+ * @param {object} props
+ * @return {ReactElement} HTML markup
+ */
 function Header(props) {
     return (
         <div>
