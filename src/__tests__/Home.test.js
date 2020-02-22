f6197214832e314d7b08645e14d0e54412e260e3
morgantatums <morgantatums@zillowgroup.com>
2020-02-21 17:36:06 -0800

    fixed home tests

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -3,14 +3,32 @@ import Home from '../views/Home';
 import {configure, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
+import { useAuth0 } from "../react-auth0-spa";
+import { BrowserRouter } from "react-router-dom";
+
+
 
 
 configure({adapter:new Adapter()});
+jest.mock('../react-auth0-spa.js');
+let auth = true;
+const  mockLogin=()=>{
+    auth=true
+  }
+
 
 describe('Snapshot Test', () => {
+    beforeEach(() => {
+        // Mock the Auth0 hook and make it return a logged in state
+        useAuth0.mockReturnValue({
+            isAuthenticated: auth,
+            loginWithRedirect: mockLogin,
+        });
+    });
     it('renders correctly, Snapshot', () => {
-        const page = renderer.create(<Home />).toJSON();
+        const page = renderer.create(<BrowserRouter><Home /></BrowserRouter>).toJSON();
         expect(page).toMatchSnapshot();
+        
     });
 });
 
